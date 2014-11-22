e0f2c02dfa3092a49434ba825aa1825f133326ac
Taher Shihadeh <taher@unixwars.com>
2014-11-22 20:28:00 +0000

    Reddit slot finder

--- dailyprogrammer/challenge-187-finding-time-to-reddit.go
+++ dailyprogrammer/challenge-187-finding-time-to-reddit.go
@@ -1,3 +1,10 @@
+/*
+http://www.reddit.com/r/dailyprogrammer/comments/2ledaj/11052014_challenge_187_intermediate_finding_time/
+[11/05/2014] Challenge #187 [Intermediate] Finding Time to Reddit
+
+Find longest slot for daily Reddit, and weekly analysis on minutes spent on tasks with percentage of time.
+*/
+
 package main
 
 import (
@@ -14,6 +21,8 @@ const (
   FILE_NAME string = "challenge-187-finding-time-to-reddit.input.txt"
   TIME_FMT string = "03:04 PM"
   BEGIN_FMT string = " 1-2-2006: " + TIME_FMT
+  DATE_FMT string = "2006-01-02 Monday"
+  REDDIT string = "reddit"
 )
 
 type Event struct {
@@ -23,19 +32,48 @@ type Event struct {
   Title string
 }
 
-type Schedule []*Event
+func (e Event) String() string {
+  return fmt.Sprintf("%s - %s (%7s) %s", e.Begin.Format(TIME_FMT), e.End.Format(TIME_FMT), e.Duration, e.Title)
+}
 
-// Schedule implements sort.Interface for []Event based on the Begin field.
-func (a Schedule) Len() int           { return len(a) }
-func (a Schedule) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a Schedule) Less(i, j int) bool { return a[i].Begin.Before(a[j].Begin) }
 
-// ByDuration implements sort.Interface for []Event based on the Duration field.
-type ByDuration Schedule
-func (a ByDuration) Less(i, j int) bool { return a[i].Duration < a[j].Duration }
+// Types and sort.Interface
+type Events []*Event
+type EventsByDate     struct { Events }
+type EventsByDuration struct { Events }
 
-func ParseFile(filename string) *Schedule{
-  var sched Schedule
+func (e Events)           Swap(i, j int)      { e[i], e[j] = e[j], e[i] }
+func (e Events)           Len() int           { return len(e) }
+func (e EventsByDate)     Less(i,j int) bool  { return e.Events[i].Begin.Before(e.Events[j].Begin) }
+func (e EventsByDuration) Less(i, j int) bool { return e.Events[i].Duration < e.Events[j].Duration }
+
+func main() {
+  events := ParseFile(FILE_NAME)
+  sort.Sort(EventsByDate{events})
+
+  day := Events{}
+
+  // Find Reddit time
+  for slicer, next := DaySlicer(events), true ; next ; {
+    day, next = slicer()
+    r := MakeRedditEvent(day)
+    events = append(events, r)
+  }
+
+  // Show days
+  sort.Sort(EventsByDate{events})
+  for slicer, next := DaySlicer(events), true ; next ; {
+    day, next = slicer()
+    fmt.Println("\nEvents for", day[0].Begin.Format(DATE_FMT))
+    ShowEvents(day)
+  }
+
+  // Stats
+  ShowStats(events)
+}
+
+func ParseFile(filename string) Events {
+  var events Events
 
   file, err := os.Open(filename)
   if err != nil {
@@ -47,9 +85,9 @@ func ParseFile(filename string) *Schedule{
 
   for scanner.Scan() {
     line := scanner.Text()
-    event,ok := ParseLine(line)
+    event,ok := ParseEvent(line)
     if ok {
-      sched = append(sched, event)
+      events = append(events, event)
     }
   }
 
@@ -57,11 +95,10 @@ func ParseFile(filename string) *Schedule{
     log.Fatal(err)
   }
 
-  sort.Sort(sched)
-  return &sched
+  return events
 }
 
-func ParseLine(line string) (*Event, bool) {
+func ParseEvent(line string) (*Event, bool) {
     event := &Event{}
 
     tokens := strings.Split(line, " to ")
@@ -87,52 +124,68 @@ func ParseLine(line string) (*Event, bool) {
 }
 
 /* 
-Returns a closure that can be used to iterate over Event slices containing Events of the same day. 
-The input is assumed sorted, so unsorted arrays might return different slices with elements of same dates.
+Generate event slices for same consecutive day
 */
-func DayIterator(events []*Event) func() ([]*Event, bool) {
+func DaySlicer(events Events) func() (Events, bool) {
     start := 0
     stop := 0
     i := 0
 
-    return func() ([]*Event, bool) {
-      for IsSameDay(events[start].Begin, events[stop].Begin) && stop < len(events) {
+    isSameDay := func(begin, end time.Time) bool {
+      return begin.Year() == end.Year() && begin.YearDay() == end.YearDay()
+    }
+
+    return func() (Events, bool) {
+      for isSameDay(events[start].Begin, events[stop].Begin) && stop < len(events)-1 {
         stop++
       }
 
       i, start = start, stop
       next := true
-      if stop == len(events) {
+      if stop == len(events)-1 {
       	 next = false
       }
-      fmt.Println("++++ ", i, start, stop, len(events), next)
       return events[i:stop], next
     }
 }
 
-func IsSameDay(begin, end time.Time) bool {
-  return begin.Year() == end.Year() && begin.YearDay() == end.YearDay()
+func MakeRedditEvent(day Events) *Event {
+  slots := Events{}
+
+  // Calculate free slots
+  for i,j := 0,1 ; j < len(day) ; i,j = i+1, j+1 {
+    duration := day[j].Begin.Sub(day[i].End)
+    event := &Event{
+      Begin: day[i].End,
+      End: day[i].End.Add(duration),
+      Duration: duration,
+      Title: REDDIT,
+    }
+    slots = append(slots, event)
+  }
+
+  // Return longest
+  sort.Sort(EventsByDuration{slots})
+  return slots[len(slots)-1]
 }
 
-func main() {
-  events := ParseFile(FILE_NAME)
-  printArray(events)
-  iterator := DayIterator(*events)
-
-  for {
-    day, next := iterator()
-    fmt.Println("\n** ", next)
-    if !next {
-      break
-    }
-    printArray(day)
+func ShowEvents(events Events) {
+  for i,e := range events {
+    fmt.Printf("%2d %s\n", i, *e)
   }
 }
 
-func printArray(events []*Event) {
-     for i,e := range events {
-     	 fmt.Println(i," ", e)
-     }
-     fmt.Println("--- Length: ", len(events))
-}
+func ShowStats(events Events) {
+  activities := make(map[string]time.Duration)
+  total := time.Duration(0)
 
+  for _,e := range events {
+    activities[e.Title] = activities[e.Title] + e.Duration
+    total += e.Duration
+  }
+
+  fmt.Printf("\nStats for total time of %s\n", total)
+  for k,v := range activities {
+    fmt.Printf("%8s (%5.2f%%)\t\t%s\n", v, float64(v)*100/float64(total), k) 
+  }
+}
