a838b7e2b8f3796febd7cfb81c047aa61d43290e
Taher Shihadeh <taher@unixwars.com>
2014-11-24 22:08:16 +0000

    Weekly placeholder

---
+++ dailyprogrammer/challenge-190-webscraping-sentiments.go
@@ -0,0 +1,6 @@
+/*
+[2014-11-24] Challenge #190 [Easy] Webscraping sentiments
+http://www.reddit.com/r/dailyprogrammer/comments/2nauiv/20141124_challenge_190_easy_webscraping_sentiments/
+*/
+
+// TODO
